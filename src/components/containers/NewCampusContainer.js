9ef580598675aa235c213d24f6e003cbaaa621ba
Adrian <mysliwiec2003@gmail.com>
2024-12-04 13:10:20 -0500

    implemented containers and updated views

---
+++ src/components/containers/NewCampusContainer.js
@@ -0,0 +1,84 @@
+import Header from "./Header";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+
+import NewCampusView from "../views/NewCampusView";
+import { addCampusThunk } from "../../store/thunks";
+
+class NewCampusContainer extends Component {
+  // Initialize state
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      address: "",
+      description: "",
+    };
+  }
+  // Capture input data when it is entered
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Take action after user click the submit button
+  handleSubmit = async (event) => {
+    event.preventDefault(); // Prevent browser reload/refresh after submit.
+
+    let campus = {
+      name: this.state.name,
+      address: this.state.address,
+      description: this.state.description,
+    };
+    if (this.state.name === "" || this.state.address === "") {
+      alert("You have not filled in all required inputs.");
+      return;
+    }
+
+    let newCampus = await this.props.addCampus(campus);
+    // Add new student in back-end database
+
+    // Update state, and trigger redirect to show the new student
+    this.setState({
+      name: "",
+      address: "",
+      description: "",
+      redirect: true,
+      redirectId: newCampus.id,
+    });
+  };
+
+  // Unmount when the component is being removed from the DOM:
+  componentWillUnmount() {
+    this.setState({ redirect: false, redirectId: null });
+  }
+
+  // Render new student input form
+  render() {
+    // Redirect to new student's page after submit
+    if (this.state.redirect) {
+      return <Redirect to={`/campus/${this.state.redirectId}`} />;
+    }
+
+    // Display the input form via the corresponding View component
+    return (
+      <div>
+        <Header />
+        <NewCampusView
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    addCampus: (campus) => dispatch(addCampusThunk(campus)),
+  };
+};
+
+export default connect(null, mapDispatch)(NewCampusContainer);
